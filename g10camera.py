40803f5f32aafd04b97806b87d8f635806c8daa7
Lasse Sjöblom <lasse@wlan-su-10-201-54-203.local.su.se>
2018-05-22 16:21:43 +0200

    ..

--- g10camera.py
+++ g10camera.py
@@ -7,29 +7,30 @@ import cv2 as cv
 import numpy as np
 from PIL import Image
 import os
-import time
+import json
 ARG_PARSER = argparse.ArgumentParser()
 ARG_PARSER.add_argument("--adduser", help="Add a new user by gathering photos")
 
 USER_PATH = "images/"
-USER_XML_PATH = "users/"
 XML_FILE = "user_info.xml"
 ARGS = ARG_PARSER.parse_args()
 
 CAM = cv.VideoCapture(0)
+USERS = {}
+if os.path.isfile('user_details.json'):
+    with open('user_details.json') as user_data:
+        USERS = json.load(user_data)
 
 DETECTOR = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 RECOGNIZER = cv.face.LBPHFaceRecognizer_create()
-if os.path.exists(XML_FILE):
-    RECOGNIZER.read(XML_FILE)
 PICTURES_TO_TAKE = 100
-
-if not os.path.exists(USER_XML_PATH):
-    os.makedirs(USER_XML_PATH)
 
 if not os.path.exists(USER_PATH):
     os.makedirs(USER_PATH)
 
+if os.path.isfile(XML_FILE):
+    RECOGNIZER.read(XML_FILE)
+    
 class AddUser:
     def __init__(self):
         self.picture = 0
@@ -37,8 +38,7 @@ class AddUser:
         self._train_system()
 
     def _train_system(self):
-        user_id = len(os.listdir(USER_PATH))
-        print(user_id)
+        user_id = len(USERS)
         faceData = []
         ids = []
         for folder in os.listdir(USER_PATH):
@@ -51,7 +51,11 @@ class AddUser:
                     for (x,y,w,h) in faces:
                         faceData.append(imageNp[y:y+h,x:x+w])
                         ids.append(user_id)
+
                 RECOGNIZER.train(faceData, np.array(ids))
+                USERS[user_id] = folder 
+                with open('user_details.json', 'w') as out:
+                    json.dump(USERS, out)
                 RECOGNIZER.save(XML_FILE)
                 shutil.rmtree(USER_PATH + folder, ignore_errors=True)
 
@@ -64,7 +68,7 @@ class AddUser:
             faces = DETECTOR.detectMultiScale(gray, 1.3, 5)
             for (x, y, w, h) in faces:
                 cv.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
-                img_path = USER_PATH + ARGS.adduser + "/" + ARGS.adduser + "_" + str(self.picture) + ".jpeg"
+                img_path = USER_PATH + ARGS.adduser + "/" + ARGS.adduser + "_" + str(self.picture) + ".png"
                 cv.imwrite(img_path, gray[y:y+h, x:x+w])
                 self.picture += 1
             cv.imshow('data_gathering', img)
@@ -87,7 +91,7 @@ class camera:
             for(x, y, w, h) in faces:
                 user, conf = RECOGNIZER.predict(gray[y:y+h, x:x+w])
                 print("USER: {} \n CONF: {}".format(user, conf))
-                cv.putText(img, "text", (x,y), self._font, self._font_scale, self._font_color, self._line_type)
+                cv.putText(img, USERS[str(user)], (x,y), self._font, self._font_scale, self._font_color, self._line_type)
             cv.imshow("camera", img)
             if cv.waitKey(10) & 0xFF == ord('q'):
                 break
